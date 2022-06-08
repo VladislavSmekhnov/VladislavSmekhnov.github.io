bc3f72661dce64221ac6ef487a19d0a942fa489b
Vlad_Sm <vladislav.smekhnov@gmail.com>
2022-06-08 16:53:12 +0300

    Added all project files

---
+++ galaxy_trvl/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import Contact from "./routers/Contact";
+import Home from "./routers/Home";
+import Pricing from "./routers/Pricing";
+import Training from "./routers/Training";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/training" element={<Training />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
